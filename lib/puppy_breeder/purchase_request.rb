4a1141ab9e5f0a27cb989fdb67e17d665981bdce
Jeff Louie <jpl7690@yahoo.com>
2014-08-29 13:02:31 -0700

    added initialize method to set breed and color for the purchase request

--- lib/puppy_breeder/purchase_request.rb
+++ lib/puppy_breeder/purchase_request.rb
@@ -1,6 +1,9 @@
 #Refer to this class as PuppyBreeder::PurchaseRequest
 module PuppyBreeder
   class PurchaseRequest
-
+  	attr_reader :breed, :color
+  	def initialize(breed, color)
+  		@breed, @color = breed, color
+  	end
   end
 end
