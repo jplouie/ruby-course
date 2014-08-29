c2448264c3a403c88601a87abf65baf2944cbbf2
Jeff Louie <jpl7690@yahoo.com>
2014-08-29 12:57:04 -0700

    created puppy class with initialize method

--- lib/puppy_breeder/puppy.rb
+++ lib/puppy_breeder/puppy.rb
@@ -1,6 +1,9 @@
 #Refer to this class as PuppyBreeder::Puppy
 module PuppyBreeder
   class Puppy
-
+  	attr_reader :breed, :color, :price
+  	initialize(breed, color, price = nil)
+  		@breed, @color, @price = breed, color, price
+  	end
   end
 end
